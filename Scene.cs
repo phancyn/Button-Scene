29d2e08ed4b65472e77b94af982891ce90759eaf
phancyn✨ <shvalinc@gmail.com>
2024-07-20 21:37:17 +0300

    Update Scene.cs

--- Scene.cs
+++ Scene.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Scene : MonoBehaviour
 {
-    public void loadScene(int sceneid)
+    public int sceneid
+    
+    public void loadScene()
     {
         SceneManager.LoadScene(sceneid);
     }
