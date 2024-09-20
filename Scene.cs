f852c339755edd4f54c5456c2baed89d90ed8c64
phancyn✨ <shvalinc@gmail.com>
2024-09-20 19:13:32 +0500

    Update Scene.cs

--- Scene.cs
+++ Scene.cs
@@ -2,11 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI
 
 public class Scene : MonoBehaviour
 {
+    //Панель настроек
+    public GameObject Panel;
+    // Для другого
+    public GameObject Panel1;
+    
     public void loadScene(int sceneid)
     {
         SceneManager.LoadScene(sceneid);
     }
+
+
+    //Открытие настроек, активация панель из UI Unity 
+    public void openSettings()
+    {
+        Panel.SetActive(true);
+    }
+
+    
+    public void closeSettings()
+    {
+        Panel.SetActive(false);
+    }
+
+    public void closeGame()
+    {
+        Application.Quit(); // закрыть приложение 
+    }
+    
+    
+    
 }
